2aa8d3db3ee27a7575e11c9f3d1aa8a396023ab1
sokkanen <joel.sokkanen@helsinki.fi>
2022-09-23 00:34:39 +0300

    Exercise 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        count = 0.0
+        for rating in self.ratings do
+            count += rating.score
+        end
+        return count / self.ratings.length
+    end
 end
