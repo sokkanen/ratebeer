324222367f0a91c99859a84bc1cda9bd16ff9ef9
sokkanen <joel.sokkanen@helsinki.fi>
2022-11-05 21:52:39 +0200

    Ex. 11

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root 'breweries#index'
   resources :beers
   resources :breweries
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to:'ratings#new'
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
-  post 'ratings', to: 'ratings#create'
 end
