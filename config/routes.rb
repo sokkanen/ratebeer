a4e3f028f4c42727443b828a25b8150c2d26c497
sokkanen <joel.sokkanen@helsinki.fi>
2022-11-09 00:15:55 +0200

    Ex. 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
   root 'breweries#index'
+
+  resources :users
   resources :beers
   resources :breweries
   resources :ratings, only: %i[index new create destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new'
-  resource :session, only: [:new, :create, :destroy]
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
