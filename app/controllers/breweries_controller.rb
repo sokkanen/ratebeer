bdf9e18507f955ee762361b44d24fd556814da2d
sokkanen <joel.sokkanen@helsinki.fi>
2022-11-16 23:31:57 +0200

    Ex 15.

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -2,7 +2,7 @@
 
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: %i[show edit update destroy]
-  before_action :authenticate, only: [:destroy]
+  before_action :ensure_that_signed_in, except: [:index, :show]
 
   # GET /breweries or /breweries.json
   def index
@@ -68,16 +68,5 @@ class BreweriesController < ApplicationController
   # Only allow a list of trusted parameters through.
   def brewery_params
     params.require(:brewery).permit(:name, :year)
-  end
-
-  def authenticate
-    admin_accounts = { 'pekka' => 'beer', 'arto' => 'foobar', 'matti' => 'ittam', 'vilma' => 'kangas' }
-    authenticate_or_request_with_http_basic do |username, password|
-      unless admin_accounts.include?(username) && (admin_accounts.fetch(username) == password)
-        raise 'Wrong username or password'
-      end
-
-      return true
-    end
   end
 end
