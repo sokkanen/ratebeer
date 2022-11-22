4e5fc8000de4a0477b5193766afe35a851fd96c2
sokkanen <joel.sokkanen@helsinki.fi>
2022-11-23 00:57:11 +0200

    Ex. 4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,11 @@
 class BeermappingApi
+
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 1.days) { get_places_in(city) }
+  end
+
+  def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
@@ -16,7 +22,6 @@ class BeermappingApi
   def self.key
     return nil if Rails.env.test?
     raise 'BEERMAPPING_APIKEY env variable not defined' if ENV['BEERMAPPING_APIKEY'].nil?
-
     ENV.fetch('BEERMAPPING_APIKEY')
   end
 end
