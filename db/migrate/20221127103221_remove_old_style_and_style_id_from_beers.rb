257a891c7cd54d68f55e12d507d9a19b3d4cd5a7
sokkanen <joel.sokkanen@helsinki.fi>
2022-11-27 14:29:44 +0200

    Adjust migration for fly.io

--- db/migrate/20221127103221_remove_old_style_and_style_id_from_beers.rb
+++ db/migrate/20221127103221_remove_old_style_and_style_id_from_beers.rb
@@ -1,7 +1,8 @@
 class RemoveOldStyleAndStyleIdFromBeers < ActiveRecord::Migration[7.0]
   def change
     add_reference :beers, :style, foreign_key: true
-    Beer.update_all(:style_id => 1)
+    s = Style.create({ :name => 'Migration', :description => 'Migration, please select correct style.'})
+    Beer.update_all(:style => s)
     remove_column :beers, :style, :string
   end
 end
