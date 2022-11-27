ed1facf745f92d09a570c1c8258d57a7a8566fa4
sokkanen <joel.sokkanen@helsinki.fi>
2022-11-27 14:20:50 +0200

    Refactoring migration.

--- db/migrate/20221127103221_remove_old_style_and_style_id_from_beers.rb
+++ db/migrate/20221127103221_remove_old_style_and_style_id_from_beers.rb
@@ -1,7 +1,7 @@
 class RemoveOldStyleAndStyleIdFromBeers < ActiveRecord::Migration[7.0]
   def change
     add_reference :beers, :style, foreign_key: true
-    Beer.update_all(:style_id => Style.first.id)
+    Beer.update_all(:style_id => 1)
     remove_column :beers, :style, :string
   end
 end
