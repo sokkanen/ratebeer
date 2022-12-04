a97ae0ee5b6ed245fd5b4f793d1768cd46102dcc
sokkanen <joel.sokkanen@helsinki.fi>
2022-12-04 13:12:24 +0200

    Ex. 11

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -6,7 +6,7 @@ class Beer < ApplicationRecord
 
   validates :name, :style, presence: true
 
-  belongs_to :brewery
+  belongs_to :brewery, touch: true
   belongs_to :style
   has_many :ratings, dependent: :destroy
   has_many :raters, -> { distinct }, through: :ratings, source: :user
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Rating < ApplicationRecord
-  belongs_to :beer
+  belongs_to :beer, touch: true
   belongs_to :user
 
   validates :score, numericality: { greater_than_or_equal_to: 1,
